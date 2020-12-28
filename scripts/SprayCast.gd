fcdd8a98b6191ebb855dcd8f482e937fb1233fe8
h00g3 <h00g3@live.at>
2020-12-28 10:46:53 +0100

    set up walls

--- scripts/SprayCast.gd
+++ scripts/SprayCast.gd
@@ -15,7 +15,18 @@ func _physics_process(delta):
 
 func _on_sprayed(collider, position, normal):
 	var tag = Sprite3D.new()
+	
+#	var tag_material = SpatialMaterial.new()
+#	tag_material.emission_enabled = true
+#	tag_material.flags_transparent = true
+	#tag_material.emission = Color(255,255,255,255)
+	
+	#tag_material.emission_texture = load("res://textures/sprayblume.png")
+	#tag_material.albedo_color = Color(255,255,255,255)
+	#tag_material.albedo_texture = load("res://textures/sprayblume.png")
+	#tag.material_override = tag_material
 	tag.texture = load("res://textures/sprayblume.png")
+
 	tag.transform.origin = position
 	var normal_correct_rotation = Vector3(normal.y, normal.x, normal.z)
 	tag.transform.basis = transform.basis.rotated(normal_correct_rotation, PI/2)
