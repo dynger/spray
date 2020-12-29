756881bfecd332b7dea65dbc0b1b56424d291e70
rr <rdssdf.fd@gmx.at>
2020-12-29 06:04:59 +0100

    align spray along surface normal

--- scripts/SprayCast.gd
+++ scripts/SprayCast.gd
@@ -4,19 +4,42 @@ extends RayCast
 
 func _physics_process(delta):
 	if Input.is_action_just_pressed("mouse_left") and is_colliding():
-		var obj = get_collider()
-		print(obj.get_name())
-		var location = get_collision_point()
+		var collider = get_collider()
+		print(collider.get_name())
+		var global_position = get_collision_point()
+#		var local_position = global_position - collider.transform.origin
+#		print(collider.transform.basis.get_rotation_quat())
 		var normal = get_collision_normal()
-		print(location)
-		print(normal)
+		#print(location)
+		#print(normal)
 #		emit_signal("sprayed", location, normal)
-		_on_sprayed(obj, location, normal)
+		_on_sprayed(collider, global_position, normal)
 
-func _on_sprayed(collider, position, normal):
+func _on_sprayed(object, collision_position, collider_normal):
 	var tag = Sprite3D.new()
+	object.add_child(tag)
+	rotate_spray_to_collider_normal(tag, collider_normal)
+	tag.texture = load("res://textures/sprayblume.png")
+	var local_position = object.to_local(collision_position)
+	tag.transform.origin = local_position# * collider_normal.normalized() * 1.01
 	
-#	var tag_material = SpatialMaterial.new()
+
+func rotate_spray_to_collider_normal(tag, collider_normal):
+	print(collider_normal)
+	var tag_front = Vector3(0,0,1)
+	
+	var axis = tag_front.cross(collider_normal)
+	axis = axis.normalized()
+
+	var cosa = tag_front.dot(collider_normal)
+	var angle = acos(cosa)
+	
+	var rotated_global_transform = tag.transform.rotated(axis, angle)
+	
+	tag.set_global_transform(rotated_global_transform) 
+
+func luminescent_material():
+	#var tag_material = SpatialMaterial.new()
 #	tag_material.emission_enabled = true
 #	tag_material.flags_transparent = true
 	#tag_material.emission = Color(255,255,255,255)
@@ -25,9 +48,4 @@ func _on_sprayed(collider, position, normal):
 	#tag_material.albedo_color = Color(255,255,255,255)
 	#tag_material.albedo_texture = load("res://textures/sprayblume.png")
 	#tag.material_override = tag_material
-	tag.texture = load("res://textures/sprayblume.png")
-
-	tag.transform.origin = position
-	var normal_correct_rotation = Vector3(normal.y, normal.x, normal.z)
-	tag.transform.basis = transform.basis.rotated(normal_correct_rotation, PI/2)
-	collider.add_child(tag)
+	pass
